75810d67445c4cc26fbf0d0fab65d2648045d622
ytoledano <ytoledano@microsoft.com>
2026-04-09 11:54:53 +0300

    Refactor devcontainer setup: remove connectedk8s extension from features and adjust post-create command for installation order

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -2,10 +2,13 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-echo "[1/2] Installing frontend dependencies..."
+echo "[1/3] Installing Azure CLI extensions..."
+az extension add --name connectedk8s --yes 2>/dev/null || true
+
+echo "[2/3] Installing frontend dependencies..."
 npm install
 
-echo "[2/2] Installing server dependencies..."
+echo "[3/3] Installing server dependencies..."
 cd server && npm install && cd ..
 
 if [ $? -eq 0 ]; then
